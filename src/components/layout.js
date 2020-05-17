18116b9e45cbb788a48eceb49db9fa16d0dd23f8
Diego Taveras <diegotaveras2@outlook.com>
2020-05-17 11:08:03 -0400

    project setup

---
+++ src/components/layout.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from './header'
+import { Helmet } from 'react-helmet';
+
+
+const Layout = (props) => {
+    return (
+        <>
+            <Helmet >
+                <title>Gatsby Hotel</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"/>
+                <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+            </Helmet>
+            <Header/>
+            {props.children}
+        </>
+      );
+}
+ 
+export default Layout;
