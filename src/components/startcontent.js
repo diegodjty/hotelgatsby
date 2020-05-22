6e1eb7a06c0ae244d909115c3429113b9e28fa54
Diego Taveras <diegotaveras2@outlook.com>
2020-05-22 00:00:32 -0400

    css to start content section

--- src/components/startcontent.js
+++ src/components/startcontent.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import Image from 'gatsby-image';
+import styled from '@emotion/styled';
+import {css} from '@emotion/core'
+
+const StartText = styled.div`
+  padding-top: 4rem;
+  max-width: 1200px;
+  width: 95%;
+  margin: 0 auto;
+
+    @media(min-width: 768px){
+      display: grid;
+      grid-template-columns: 2fr 1fr;
+      column-gap: 100px;
+
+    }
+    p{
+      line-height: 2;
+    }
+`
+  
 
 const StartContent = () => {
 
@@ -26,11 +46,12 @@ const StartContent = () => {
 
     return (
         <>
-            <h2>{title}</h2>
-            <div>
+            <h2 css={css`text-align: center;font-size: 4rem;margin-top: 4rem;`}>{title}</h2>
+            <StartText>
               <p>{content}</p>
-            </div>
-            <Image fluid={image.fluid} alt=""/>
+              <Image fluid={image.fluid} alt=""/>
+            </StartText>
+            
         </>
       );
 }
